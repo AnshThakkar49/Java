d46146de471d45f7a2ebdf631c82458b185b8de7
AnshThakkar49 <148559109+AnshThakkar49@users.noreply.github.com>
2024-02-29 23:02:06 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+//23-02-2024
+public class Sum {
+    public static void main(String[] args) 
+    {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        int a,b,sum;
+        a=Integer.parseInt(args[0]);
+        b=Integer.parseInt(args[1]);
+        sum= a+b;
+        System.out.println(sum);
+    }
+}
